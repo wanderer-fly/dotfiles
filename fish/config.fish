788de0603e0b24f62181312bf1c3988b8203ede5
wanderer-fly <wand.liulang@gmail.com>
2025-02-16 18:18:02 +0800

    dc

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,5 @@
+alias discord '/usr/bin/discord --enable-features=UseOzonePlatform --ozone-platform=wayland' # Fuck u Discord
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
